d3eafdd80aee580d3aa21b956147cb3eb7d428e1
unknown <chiedoziedavidehirim@gmail.com>
2023-12-16 12:57:29 +0100

    Little Addition

--- bank_project/customers/views.py
+++ bank_project/customers/views.py
@@ -29,9 +29,11 @@ def open_account(request):
 def profile(request):
     return render(request, 'customers/profile.html') 
 
+@login_required
 def confirm_logout(request):
     return render(request, 'customers/confirm_logout.html')
 
+login_required
 def transfer(request):
     if request.method == 'POST':
         form = TransferForm(request.POST)
