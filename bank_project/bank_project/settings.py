d055dd59cbf1d7056b38835b8732ea867e21be40
unknown <chiedoziedavidehirim@gmail.com>
2024-04-25 00:16:16 +0100

    commit

--- bank_project/bank_project/settings.py
+++ bank_project/bank_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jisxvfcq+8@929cy^e*@a!yo8fl4pmw-94hph7-&@v1l$+wi71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
